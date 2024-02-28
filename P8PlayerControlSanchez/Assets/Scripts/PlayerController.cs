bc7445c5986c462e2494f0705e70aca9b2f396f7
Adam Sanchez <722945@student.dusd.net>
2024-02-28 15:26:08 -0800

    added more stuf
    added in more boxes as an obstacle, made the camera stay being the player at all times, and made it so that the player can move left to right (slowly might want to and might change values to be higher later.)

--- P8PlayerControlSanchez/Assets/Scripts/PlayerController.cs
+++ P8PlayerControlSanchez/Assets/Scripts/PlayerController.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+   public float speed = 5.0f;
+    public float turnSpeed;
+    public float HorizontalInput;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,9 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        HorizontalInput = Input.GetAxis("Horizontal");
+        //Move the vehicle forward
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        transform.Translate(Vector3.right * Time.deltaTime * turnSpeed * HorizontalInput);
     }
 }
